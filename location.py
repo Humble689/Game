62ad7332bf17852416b5450172c33964108f259e
Humble689 <marktravis689@gmail.com>
2025-06-11 13:01:02 +0300

    location

--- location.py
+++ location.py
@@ -4,3 +4,10 @@ class Loaction:
         self.description = description
         self.enemy = enemy
     
+
+    def enter(self, player):
+        print(f"\nYou enter {self.nmae}: {self.description}")
+        if self.enemy:
+            combat(player, self.enemy)
+        else:
+            print (f"{self.name} is a peacefu place. Nothing happens here.")
