599eecc662dc86b4143f12275919e62222884bd9
Humble689 <marktravis689@gmail.com>
2025-06-11 12:51:41 +0300

    inventory

--- Inventory.py
+++ Inventory.py
@@ -1,2 +1,7 @@
 class Item:
-    def __init__(self, name, type. effect):
+    def __init__(self, name, type, effect):
+        self.name = name
+        self.type = type
+        self.effect = effect
+
+    def use (self, player):
