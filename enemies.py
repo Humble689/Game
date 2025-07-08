7bbdb5edca2f1cb627cd03c962b0da06d3136d9b
Travis <146099262+Humble689@users.noreply.github.com>
2025-07-08 19:07:26 +0300

    Update enemies.py

--- enemies.py
+++ enemies.py
@@ -2,8 +2,8 @@ class Enemy:
     def __init__(self, name, health, strength, defense):
         self.name = name
         self. health = health
-        self.strength = strength
-        self.defense = defense
+        self.strength = strength # strength
+        self.defense = defense #
 
 
     def attack(self, player):
